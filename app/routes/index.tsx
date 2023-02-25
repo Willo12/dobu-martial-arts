a4620f5870847ba775ee92d0061c1bea34176ddf
BrazlyBearWolf <merive33+github@gmail.com>
2023-02-25 10:51:29 +0000

    made the navbar animate up when  scrolling

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,6 +4,50 @@ export default function Index() {
   return (
     <>
       <Navbar />
+      <div>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+        <h1 className="text-9xl">hi hi hi hi hi hi hi hi hi hi hi hi hi hi</h1>
+      </div>
     </>
   );
 }
